5d302b85b9fb22e79f49c258d88bddb76c1a3230
pikv <trololol@gamail.com>
2019-04-03 07:44:11 -0600

    Errorsillo areglado

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './components/event-list/event-list.component';
 import { EventoServiceService } from '../app/services/evento/evento-service.service';
-import { DealerComponent } from './components/dealer/dealer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
-    DealerComponent,
     NavbarComponent
   ],
   imports: [
